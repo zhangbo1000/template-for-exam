056b04cd3dbb678f0c95c2adc4595f577b6331b4
zhangbo1000 <165640986+zhangbo1000@users.noreply.github.com>
2024-08-06 16:08:18 +0800

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,4 +1,3 @@
-//Please remember it instead of copy.
 
 #include<iostream>
 #include<cstdio>
@@ -32,11 +31,8 @@ namespace code{
 	_Tp min(const _Tp& x,const _Rest&... rest){
 		return min(x,min(rest...));
 	}
-	char gtc(){
-		static char tmp[100000],*p1(0),*p2(0);
-		if(p1==p2)p2=(p1=tmp)+fread(tmp,1,100000,stdin);
-		return *p1++;
-	}
+	char tmp[100000],*p=0,*top=0;
+	#define gtc() ((p==top)&&(top=(p=tmp)+fread(tmp,1,100000,stdin)),*p++)
 	void read(int& x){
 		char c;
 		while((c=gtc())<'0');
